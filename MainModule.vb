2929bef5ce0b0d28dce9f72850760a74b3e92440
rosstimo <rosstimo@isu.edu>
2021-02-08 08:24:44 -0700

    add random number function

--- MainModule.vb
+++ MainModule.vb
@@ -2,8 +2,14 @@
 
     Sub Main()
         Console.WriteLine(libTJR.SayHello())
-
+        For i = 0 To 5
+            Console.WriteLine(libTJR.GetRandomNumber(5))
+        Next
         Console.Read()
     End Sub
+
+
+
+
 
 End Module
--- libTJR.vb
+++ libTJR.vb
@@ -4,7 +4,10 @@
         Return "Hello, World!"
     End Function
 
-
+    Function GetRandomNumber(maxNumber As Integer) As Integer
+        Randomize(DateTime.Now.Millisecond)
+        Return CInt(Math.Floor(Rnd() * (maxNumber + 1)))
+    End Function
 
 
 End Module
