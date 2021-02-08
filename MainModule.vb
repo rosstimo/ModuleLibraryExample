b11b285f3990e19dc0e296c0f4727e6ca6d43877
rosstimo <rosstimo@isu.edu>
2021-02-08 08:51:10 -0700

    add docstrings

--- MainModule.vb
+++ MainModule.vb
@@ -5,11 +5,9 @@
         For i = 0 To 5
             Console.WriteLine(libTJR.GetRandomNumber(5))
         Next
+        libTJR.GetRandomNumber(5)
+
         Console.Read()
     End Sub
-
-
-
-
 
 End Module
--- libTJR.vb
+++ libTJR.vb
@@ -1,9 +1,24 @@
-﻿Module libTJR
+﻿
 
+
+''' <summary>
+''' A collection of useful though somewhat eccentric methods and resource thingies...
+''' </summary>
+Module libTJR
+
+    ''' <summary>
+    ''' Says Hello
+    ''' </summary>
+    ''' <returns>String: "Hello, World" </returns>
     Function SayHello() As String
         Return "Hello, World!"
     End Function
 
+    ''' <summary>
+    ''' Generates a random integer within a range
+    ''' </summary>
+    ''' <param name="maxNumber"></param>
+    ''' <returns>Integer value from 0 to maxNumber inclusive.</returns>
     Function GetRandomNumber(maxNumber As Integer) As Integer
         Randomize(DateTime.Now.Millisecond)
         Return CInt(Math.Floor(Rnd() * (maxNumber + 1)))
